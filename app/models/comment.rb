f6b73d10f2b380499bb8b028a4a99d7ce513a8c5
SWillames <sergiowoc@gmail.com>
2019-11-19 00:36:33 -0300

    adding data relations.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,3 @@
 class Comment < ApplicationRecord
-    has_many :comments, dependent: :destroy
+    belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :post
+    has_many :comments, dependent: :destroy
+
+    validates_presence_of :title
 end
